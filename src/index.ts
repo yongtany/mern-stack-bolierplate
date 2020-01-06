28e1219b3d5052c48b77016f692f9918c2d53370
yongtany <yongn1494@gmail.com>
2020-01-06 15:24:05 +0900

    [CREATE] Database settings

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+
 import { router } from './routes/users';
+const config = require("./config/keys");
 
 const app = express();
+
+mongoose
+  .connect(config.mongoURI, { useNewUrlParser: true })
+  .then(() => console.log("DB connected"))
+  .catch(err => console.error(err));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
